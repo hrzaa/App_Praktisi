413c4ebf60ddc69f803325de756d69de2a503342
MujahidRopiq <92687253+MujahidRopiq@users.noreply.github.com>
2022-11-05 01:21:21 +0700

    add halaman ropik

---
+++ app/src/main/java/com/example/app_praktisi/RopiqActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_praktisi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RopiqActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ropiq);
+    }
+}
